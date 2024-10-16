2263ed48dfe24d4057b28e06a3ffdf455272bc89
M9-GBR <150633267+M9-GBR@users.noreply.github.com>
2024-10-16 03:36:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -41,3 +41,21 @@ function addStar(elem, amt) {
 }
 
 addRatings()
+
+let navbar = document.querySelector('nav'),
+    currentY = 0,
+    lastY = currentY
+
+document.addEventListener('scroll', ev => {
+    currentY = window.scrollY
+
+    if (Math.abs(currentY - lastY) != currentY - lastY) {
+        navbar.classList.remove('navbar-hide')
+        navbar.classList.add('navbar-show')
+    } else if (Math.abs(currentY - lastY) == currentY - lastY) {
+        navbar.classList.remove('navbar-show')
+        navbar.classList.add('navbar-hide')
+    }
+
+    lastY = currentY
+})
